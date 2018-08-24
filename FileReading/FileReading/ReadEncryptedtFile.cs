d5015fc413ad885a26f2a7750c4b1e09628bf600
Victor Dushi <victor.dushi@yahoo.fr>
2018-08-24 11:22:24 +0200

    FileReadingV3

---
+++ FileReading/FileReading/ReadEncryptedtFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReading
+{
+    public class ReadEncryptedtFile : ReadFile, IReadEncryptedFile
+    {
+        public override object ReadingdFiles(string fileLocation)
+        {
+            // Implementation of Encrypted files to finish 
+
+            byte[] fileBytes = File.ReadAllBytes(fileLocation);
+
+            return fileBytes;
+        }
+
+    }
+}
